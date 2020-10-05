6f7ca16fb4b115e28322065cc5a5e0cd37b4af90
Jishnu Saurav Mittapalli <55013611+jishnusaurav@users.noreply.github.com>
2020-10-06 03:08:04 +0530

    Create reverLL.cpp

---
+++ Geeksforgeeks/DEShaw/reverLL.cpp
@@ -0,0 +1,17 @@
+
+struct Node* reverseList(struct Node *head)
+{
+    // code here
+    // return head of reversed list
+    Node* current=head;
+    Node* previous = NULL;
+    Node* next = NULL;
+    while(current!=NULL){
+        next=current->next;
+        current->next=previous;
+        previous = current;
+        current=next;
+    }
+    head=previous;
+    return head;
+}
